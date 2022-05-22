5809e828ca5fe779e08dad18b11e92767622184c
VidalTorres <vidaltorres2001@outlook.com>
2022-05-22 17:09:31 -0600

    Proyecto terminado

---
+++ app/src/main/java/com/example/boleto/Boleto.java
@@ -0,0 +1,158 @@
+package com.example.boleto;
+
+import java.util.Scanner;
+
+public class Boleto {
+    //variables
+    private int numeroBoleto;
+    private String nombreCliente;
+    private String destino;
+    private int tipoBoleto;
+    private String fecha;
+    private double precio;
+
+    private double impuesto;
+    private double descuento;
+    private double subtotal;
+    private double total;
+
+    //Constructores
+    public Boleto(){
+        this.numeroBoleto = 0;
+        this.nombreCliente = "";
+        this.destino = "";
+        this.tipoBoleto = 0;
+        this.fecha = "";
+        this.precio = 0.0;
+    }
+
+    public Boleto(int numeroBoleto, String nombreCliente, String destino, int tipoBoleto, String fecha, double precio){
+        this.numeroBoleto = numeroBoleto;
+        this.nombreCliente = nombreCliente;
+        this.destino = destino;
+        this.tipoBoleto = tipoBoleto;
+        this.fecha = fecha;
+        this.precio = precio;
+    }
+
+    public Boleto(Boleto boleto){
+        this.numeroBoleto = boleto.numeroBoleto;
+        this.nombreCliente = boleto.nombreCliente;
+        this.destino = boleto.destino;
+        this.tipoBoleto = boleto.tipoBoleto;
+        this.fecha = boleto.fecha;
+        this.precio = boleto.precio;
+    }
+
+
+
+    //Encapsulamiento
+    public int getNumeroBoleto() {
+        return numeroBoleto;
+    }
+
+    /**
+     * @param numeroBoleto the numeroBoleto to set
+     */
+    public void setNumeroBoleto(int numeroBoleto) {
+        this.numeroBoleto = numeroBoleto;
+    }
+
+    /**
+     * @return the nombreCliente
+     */
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    /**
+     * @param nombreCliente the nombreCliente to set
+     */
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    /**
+     * @return the destino
+     */
+    public String getDestino() {
+        return destino;
+    }
+
+    /**
+     * @param destino the destino to set
+     */
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    /**
+     * @return the tipoBoleto
+     */
+    public int getTipoBoleto() {
+        return tipoBoleto;
+    }
+
+    /**
+     * @param tipoBoleto the tipoViaje to set
+     */
+    public void setTipoBoleto(int tipoBoleto) {
+        this.tipoBoleto = tipoBoleto;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public String getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    //Funciones
+    public double obtenerSubtotal(){
+        if(this.tipoBoleto==1) subtotal = this.precio;
+        if(this.tipoBoleto==2) subtotal = this.precio*1.8;
+        return subtotal;
+    }
+
+    public double obtenerDescuento(int edad){
+        if (edad > 60){
+            descuento = subtotal/2; //Entre 2 porque es el 50% de descuento
+        }
+        return descuento;
+    }
+
+    public double obtenerImpuesto(){
+        impuesto = subtotal * .16; //Obtener especificamente el monto de impuesto
+        subtotal = subtotal + impuesto; //agreagarle el impuesto al subotal
+        return impuesto;
+    }
+
+    public double obtenerTotal(){
+        total = subtotal - descuento;
+        return total;
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/boleto/MainActivity.java
@@ -0,0 +1,178 @@
+package com.example.boleto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNumeroBoleto;
+    private EditText txtNombreCliente;
+    private EditText txtEdad;
+    private EditText txtFecha;
+    private Spinner spnDestinos;
+    private Spinner spnTipoBoleto;
+    private EditText txtPrecio;
+
+    private Button btnCalular;
+    private Button btnLimpiar;
+
+    private TextView lblNumeroBoleto2;
+    private TextView lblNombreCliente2;
+    private TextView lblEdad2;
+    private TextView lblFecha2;
+    private TextView lblDestinos2;
+    private TextView lblTipoBoleto2;
+    private TextView lblPrecio2;
+    private TextView lblSubTotal;
+    private TextView lblTotal;
+    private TextView lblDescuento;
+    private TextView lblImpuesto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lblNumeroBoleto2 = (TextView) findViewById(R.id.lblNumeroBoleto2);
+        lblNombreCliente2 = (TextView) findViewById(R.id.lblNombreCliente2);
+        lblEdad2 = (TextView) findViewById(R.id.lblEdad2);
+        lblFecha2 = (TextView) findViewById(R.id.lblFecha2);
+        lblDestinos2 = (TextView) findViewById(R.id.lblDestinos2);
+        lblTipoBoleto2 = (TextView) findViewById(R.id.lblTipoBoleto2);
+        lblPrecio2 = (TextView) findViewById(R.id.lblPrecio2);
+
+        lblSubTotal = (TextView) findViewById(R.id.lblSubTotal);
+        lblTotal = (TextView) findViewById(R.id.lblTotal);
+        lblDescuento = (TextView) findViewById(R.id.lblDescuento);
+        lblImpuesto = (TextView) findViewById(R.id.lblImpuesto);
+
+        txtNumeroBoleto = (EditText) findViewById(R.id.txtNumeroBoleto);
+        txtNombreCliente = (EditText) findViewById(R.id.txtNombreCliente);
+        txtEdad = (EditText) findViewById(R.id.txtEdad);
+        txtFecha = (EditText) findViewById(R.id.txtFecha);
+        spnDestinos = (Spinner) findViewById(R.id.spnDestinos);
+        spnTipoBoleto = (Spinner) findViewById(R.id.spnTipoBoleto);
+        txtPrecio = (EditText) findViewById(R.id.txtPrecio);
+
+        btnCalular = (Button) findViewById(R.id.btnCalcular);
+        btnLimpiar = (Button) findViewById(R.id.btnLimpiar);
+
+        final String [] destinos = {""};
+        final int [] tipoBoleto = {0};
+
+        Boleto boleto = new Boleto();
+
+        btnCalular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double subTotal;
+                double descuento;
+                double impuesto;
+                double total;
+
+                String numeroBoleto = txtNumeroBoleto.getText().toString();
+                boleto.setNumeroBoleto(Integer.parseInt(numeroBoleto));
+
+                String nombreCliente = txtNombreCliente.getText().toString();
+                boleto.setNombreCliente(nombreCliente);
+
+                String edad = txtEdad.getText().toString();
+
+                String fecha = txtFecha.getText().toString();
+                boleto.setFecha(fecha);
+
+                boleto.setDestino(destinos[0]);
+
+                boleto.setTipoBoleto(tipoBoleto[0]);
+
+                String precio = txtPrecio.getText().toString();
+                boleto.setPrecio(Double.parseDouble(precio));
+
+                subTotal = boleto.obtenerSubtotal(); //crear textView
+
+                descuento = boleto.obtenerDescuento(Integer.parseInt(edad));
+
+                impuesto = boleto.obtenerImpuesto();
+
+                total = boleto.obtenerTotal();
+
+                //Mostrar datos
+                lblNumeroBoleto2.setText("# " + boleto.getNumeroBoleto());
+                lblNombreCliente2.setText("Nombre: " + boleto.getNombreCliente());
+                lblEdad2.setText("Edad " + edad);
+                lblFecha2.setText("Fecha: "+ boleto.getFecha());
+                lblDestinos2.setText("Destino: " + boleto.getDestino());
+                lblTipoBoleto2.setText("Tipo Boleto: "+ boleto.getTipoBoleto());
+                lblPrecio2.setText("Precio: " + boleto.getPrecio());
+                lblSubTotal.setText("Subtotal: "+subTotal);
+                lblImpuesto.setText("Impuesto: " + impuesto);
+                lblDescuento.setText("Descuento: " + descuento);
+                lblTotal.setText("Total: " + total);
+            }
+        });
+
+        ArrayAdapter<String> Adpt1 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_expandable_list_item_1,getResources().getStringArray(R.array.destinos));
+        spnDestinos.setAdapter(Adpt1);
+        spnDestinos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                destinos[0] = adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<String> Adpt2 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_expandable_list_item_1,getResources().getStringArray(R.array.tipoBoleto));
+        spnTipoBoleto.setAdapter(Adpt2);
+        spnTipoBoleto.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String tipo = adapterView.getItemAtPosition(i).toString();
+                if (tipo.matches("Sencillo")) {
+                    tipoBoleto [0] = 1;
+                } else if (tipo.matches("Doble")){
+                    tipoBoleto [0] = 2;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        btnLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtNumeroBoleto.setText("");
+                txtNombreCliente.setText("");
+                txtEdad.setText("");
+                txtFecha.setText("");
+                txtPrecio.setText("");
+
+                lblNumeroBoleto2.setText("");
+                lblNombreCliente2.setText("");
+                lblEdad2.setText("");
+                lblFecha2.setText("");
+                lblDestinos2.setText("");
+                lblTipoBoleto2.setText("");
+                lblPrecio2.setText("");
+                lblSubTotal.setText("");
+                lblImpuesto.setText("");
+                lblDescuento.setText("");
+                lblTotal.setText("");
+            }
+        });
+    }
+}
